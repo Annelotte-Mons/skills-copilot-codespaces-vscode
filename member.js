ef002b8522e0eb8b555319e3c2a0858590249143
Annelotte Mons <43070906+Annelotte-Mons@users.noreply.github.com>
2023-11-20 16:34:03 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function SkillsMember() {
+    this.id = 0;
+    this.name = "";
+    this.skills = [];
+    this.addSkill = function (skill) {
+        this.skills.push(skill);
+    }
+    this.removeSkill = function (skill) {
+        var index = this.skills.indexOf(skill);
+        if (index > -1) {
+            this.skills.splice(index, 1);
+        }
+    }
+    this.toString = function () {
+        return this.name;
+    }
+
+}
